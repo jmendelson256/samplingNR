5e36632c0d9ca3aa8ddfc0602d6db3f1965f43aa
Jonathan Mendelson <jmendelson@gmail.com>
2023-10-18 19:29:45 -0400

    add PEVS ADM response rate data

---
+++ R/samplingNR-datasets.r
@@ -0,0 +1,57 @@
+#' PEVS-ADM 2016 response rate information by poststrata
+#'
+#'@description
+#'Population and response rate information for the Federal Voting Assistance
+#'Program's 2016 Post-Election Voting Survey
+#'of U.S. Active Duty Military (PEVS-ADM; FVAP, 2017a).
+#'
+#'@details
+#'This table is based on secondary analysis of survey data from
+#'FVAP's 2016 PEVS-ADM.
+#'The sampling frame reflects U.S. Active Duty Military, as of
+#'July 2016, based on administrative data from the
+#'U.S. Defense Manpower Data Center (DMDC); see FVAP's technical
+#'report (2017b) for details.
+#'Our analysis exclusively examines the control group
+#'from FVAP's response rate experiment (\eqn{(n = 77{,}333)});
+#'we further omit \eqn{1{,}785} sample members who were identified as ineligible
+#'via administative data prior to survey fielding; these
+#'units were in the July 2016 sampling frame, but were identified via
+#'September 2016 personnel records as no longer being on active duty
+#'(e.g., due to separations or retirements).
+#'Population sizes were estimated from the survey's
+#'base weights for the remaining cases.
+#'
+#'Poststratum response propensities are base-weighted
+#'proportions of invitees who provided an eligible, complete response to the
+#'survey; the denominator includes all invited units
+#'(as opposed to excluding ineligibles, as in AAPOR response rates),
+#'so that resulting sample allocation calculations will reflect the
+#'effects of sample loss on the costs of data collection.
+#'
+#' @format Tibble containing 91 observations and 8 variables. Includes:
+#' \describe{
+#'     \item{\code{h}}{(chr) Poststrata}
+#'     \item{\code{service}}{(fct) Military service}
+#'     \item{\code{paygrade}}{(fct) Pay grade category}
+#'     \item{\code{age}}{(fct) Age category}
+#'     \item{\code{region}}{(fct) Region}
+#'     \item{\code{sex}}{(chr) Sex}
+#'     \item{\code{Nhat_h}}{(dbl) Estimated population size in poststratum h}
+#'     \item{\code{rr_h}}{(dbl) Base-weighted empirical response propensity in poststratum h}
+#' }
+#'@keywords data
+#'@docType data
+#'@references
+#'Federal Voting Assistance Program. (2017a). 2015–2016 Active Duty Military dataset.
+#'Federal Voting Assistance Program, U.S. Department of Defense.
+#'\url{https://www.fvap.gov/uploads/FVAP/Surveys/PEV51601_Pub.dta} [Accessed 17 January 2023].
+#'
+#'Federal Voting Assistance Program (2017b). Post-Election Voting Surveys:
+#'Active Duty Military: Technical report 2016.
+#'Technical report, Federal Voting Assistance Program,
+#'U.S. Department of Defense.
+#'\url{https://www.fvap.gov/uploads/FVAP/Reports/PEVS_ADM_TechReport_Final.pdf}
+#'[Accessed 17 January 2023].
+"pevs_adm_2016_rr"
+
---
+++ data-raw/import_RR_table_from_paper.R
@@ -0,0 +1,26 @@
+#RR table in paper was generated here:
+#  C:\Users\jmend\Documents\JPSM\Dissertation\R packages\jmpaper3\examples-long\PEVS_ADM_2016
+#See file:///C:/Users/jmend/Documents/JPSM/Dissertation/R%20packages/jmpaper3/examples-long/PEVS_ADM_2016/ADM_11_new_setup_20230424.html
+#  Environment including RR table saved to env11_pevs_adm.rds
+
+##C:/Users/jmend/Documents/JPSM/Dissertation/Writeups/Paper 3 - Nonresponse and design/Math/stratified domain calcs/domain_est_stratified_recodes.R"
+
+library(magrittr)
+library(dplyr)
+
+env11_pevs_adm <- readRDS("C:/Users/jmend/Documents/JPSM/Dissertation/R packages/samplingNR/data-raw/env11_pevs_adm.rds")
+
+pevs_adm_2016_rr <-
+  env11_pevs_adm$fvap_alloc50_table_for_output_with_total[1:91,] %>%
+  select(h:rr_h) %>%
+  rename(service = eps_svc,
+         paygrade = eps_pg,
+         age = eps_age,
+         region = eps_region,
+         sex = eps_sex,
+         Nhat_h = N_h,
+         rr_h = rr_h) %>%
+  mutate(h = as.character(h))
+
+#usethis::use_data(pevs_adm_2016_rr, overwrite = TRUE)
+usethis::use_data(pevs_adm_2016_rr)
